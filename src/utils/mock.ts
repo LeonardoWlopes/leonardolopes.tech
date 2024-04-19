6d6b5c02f9ee139ef5ab320ca8c7683d090af9cb
Leonardo Lopes <leonardo@buildbox.com.br>
2024-04-19 13:00:03 -0300

    feat: created tech categories

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -1,7 +1,7 @@
 // types
 import type { IContactLink } from '@/interfaces/contact'
-import type { ITechCardProps } from '@/components/tech-card'
-import { ETechCategory } from '@/enums/tech'
+import type { ITechCardList } from '@/interfaces/tech'
+import { ETechCategory, ETechTag } from '@/enums/tech'
 
 //assets
 import javascript from '@/assets/icons/javascript.svg'
@@ -28,161 +28,203 @@ import vitest from '@/assets/icons/vitest.svg'
 import jest from '@/assets/icons/jest.svg'
 import cypress from '@/assets/icons/cypress.svg'
 import docker from '@/assets/icons/docker.svg'
+import rust from '@/assets/icons/rust.svg'
+import bun from '@/assets/icons/bun.svg'
 import { Linkedin, Github, Mail } from 'lucide-react'
 
 // utils
 import { env } from '@/env'
 
-export const DEVELOPMENT_CARDS: ITechCardProps[] = [
+export const DEVELOPMENT_CARDS: ITechCardList[] = [
 	{
 		name: 'Javascript',
 		image: javascript,
-		category: ETechCategory.LANGUAGE,
+		tag: ETechTag.LANGUAGE,
 		link: 'https://developer.mozilla.org/pt-BR/docs/Web/JavaScript',
+		category: ETechCategory.LANGUAGE,
 	},
 	{
 		name: 'Typescript',
 		image: typescript,
-		category: ETechCategory.LANGUAGE,
+		tag: ETechTag.LANGUAGE,
 		link: 'https://www.typescriptlang.org/',
+		category: ETechCategory.LANGUAGE,
 	},
 	{
 		name: 'Python',
 		image: python,
-		category: ETechCategory.LANGUAGE,
+		tag: ETechTag.LANGUAGE,
 		link: 'https://www.python.org/',
+		category: ETechCategory.LANGUAGE,
 	},
+	{
+		name: 'Rust',
+		image: rust,
+		tag: ETechTag.LANGUAGE,
+		link: 'https://www.rust-lang.org/',
+		category: ETechCategory.LANGUAGE,
+	},
+
 	{
 		name: 'Vite',
 		image: vite,
-		category: ETechCategory.TOOL,
+		tag: ETechTag.TOOL,
 		link: 'https://vitejs.dev/',
+		category: ETechCategory.TOOL,
 	},
 	{
 		name: 'React',
 		image: react,
-		category: ETechCategory.LIBRARY,
+		tag: ETechTag.LIBRARY,
 		link: 'https://reactjs.org/',
+		category: ETechCategory.LIBRARY,
 	},
 	{
 		name: 'Next JS',
 		image: next,
-		category: ETechCategory.FRAMEWORK,
+		tag: ETechTag.FRAMEWORK,
 		link: 'https://nextjs.org/',
+		category: ETechCategory.FRAMEWORK,
 	},
 	{
 		name: 'Tailwind CSS',
 		image: tailwind,
-		category: ETechCategory.CSS,
+		tag: ETechTag.CSS,
 		link: 'https://tailwindcss.com/',
+		category: ETechCategory.LIBRARY,
 	},
 	{
 		name: 'Styled Components',
 		image: styled,
-		category: ETechCategory.FRAMEWORK,
+		tag: ETechTag.LIBRARY,
 		link: 'https://styled-components.com/',
+		category: ETechCategory.LIBRARY,
 	},
 	{
 		name: 'React Native',
 		image: react,
-		category: ETechCategory.MOBILE,
+		tag: ETechTag.MOBILE,
 		link: 'https://reactnative.dev/',
+		category: ETechCategory.FRAMEWORK,
 	},
 	{
 		name: 'Expo',
 		image: expo,
-		category: ETechCategory.MOBILE,
+		tag: ETechTag.MOBILE,
 		link: 'https://expo.dev/',
+		category: ETechCategory.TOOL,
 	},
 	{
 		name: 'Hook Form',
 		image: hookForm,
-		category: ETechCategory.VALIDATION,
+		tag: ETechTag.VALIDATION,
 		link: 'https://react-hook-form.com/',
+		category: ETechCategory.LIBRARY,
 	},
 	{
 		name: 'React Query',
 		image: reactQuery,
-		category: ETechCategory.GLOBAL_STATE,
+		tag: ETechTag.GLOBAL_STATE,
 		link: 'https://react-query.tanstack.com/',
+		category: ETechCategory.LIBRARY,
 	},
 	{
 		name: 'Zustand',
 		image: zustand,
-		category: ETechCategory.GLOBAL_STATE,
+		tag: ETechTag.GLOBAL_STATE,
 		link: 'https://zustand-demo.pmnd.rs/',
+		category: ETechCategory.LIBRARY,
 	},
 	{
 		name: 'Zod',
 		image: zod,
-		category: ETechCategory.VALIDATION,
+		tag: ETechTag.VALIDATION,
 		link: 'https://zod.dev/',
+		category: ETechCategory.LIBRARY,
 	},
 	{
 		name: 'Node',
 		image: node,
-		category: ETechCategory.RUNTIME,
+		tag: ETechTag.RUNTIME,
 		link: 'https://nodejs.org/',
+		category: ETechCategory.RUNTIME,
+	},
+	{
+		name: 'Bun',
+		image: bun,
+		tag: ETechTag.RUNTIME,
+		link: 'https://bun.sh/',
+		category: ETechCategory.RUNTIME,
 	},
 	{
 		name: 'Express',
 		image: express,
-		category: ETechCategory.FRAMEWORK,
+		tag: ETechTag.FRAMEWORK,
 		link: 'https://expressjs.com/',
+		category: ETechCategory.FRAMEWORK,
 	},
 	{
 		name: 'Fastify',
 		image: fastify,
-		category: ETechCategory.FRAMEWORK,
+		tag: ETechTag.FRAMEWORK,
 		link: 'https://www.fastify.io/',
+		category: ETechCategory.FRAMEWORK,
 	},
 	{
 		name: 'Nest',
 		image: nest,
-		category: ETechCategory.FRAMEWORK,
+		tag: ETechTag.FRAMEWORK,
 		link: 'https://nestjs.com/',
+		category: ETechCategory.FRAMEWORK,
 	},
 	{
 		name: 'Prisma',
 		image: prisma,
-		category: ETechCategory.ORM,
+		tag: ETechTag.ORM,
 		link: 'https://www.prisma.io/',
+		category: ETechCategory.DATABASE,
 	},
 	{
 		name: 'MongoDB',
 		image: mongo,
-		category: ETechCategory.DATABASE,
+		tag: ETechTag.DATABASE,
 		link: 'https://www.mongodb.com/',
+		category: ETechCategory.DATABASE,
 	},
 	{
 		name: 'PostgresSQL',
 		image: postgres,
-		category: ETechCategory.DATABASE,
+		tag: ETechTag.DATABASE,
 		link: 'https://www.postgresql.org/',
+		category: ETechCategory.DATABASE,
 	},
 	{
 		name: 'Docker',
 		image: docker,
-		category: ETechCategory.TOOL,
+		tag: ETechTag.TOOL,
 		link: 'https://www.docker.com/',
+		category: ETechCategory.TOOL,
 	},
 	{
 		name: 'Jest',
 		image: jest,
-		category: ETechCategory.TESTING,
+		tag: ETechTag.TESTING,
 		link: 'https://jestjs.io/',
+		category: ETechCategory.TESTING,
 	},
 	{
 		name: 'Vitest',
 		image: vitest,
-		category: ETechCategory.TESTING,
+		tag: ETechTag.TESTING,
 		link: 'https://vitest.dev/',
+		category: ETechCategory.TESTING,
 	},
 	{
 		name: 'Cypress',
 		image: cypress,
-		category: ETechCategory.TESTING,
+		tag: ETechTag.TESTING,
 		link: 'https://www.cypress.io/',
+		category: ETechCategory.TESTING,
 	},
 ]
 
